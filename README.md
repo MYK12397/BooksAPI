05231af0cf28b09d70017d05ff642db04be4c83f
Mohd Yahiya Khan <yusufzai.12397@gmail.com>
2023-02-02 12:58:34 +0530

    README file added

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Book Management Application
+
+## Overview
+
+Book Management is a backend service.
+
+## API Documentation
+To run this service on your local machine, cd to this [path](https://github.com/MYK12397/BooksAPI/tree/master/cmd/main) and run below commands:
+>  go build 
+go run main.go
+
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,13 +1,16 @@
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 var (
 	db *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:Root@123/books?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:Root@123@/books?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
